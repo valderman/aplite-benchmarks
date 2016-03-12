78f1e5386e4ded6936ceba41be4f112206b442ca
Anton Ekblad <anton@ekblad.cc>
2016-03-12 16:47:40 +0100

    Add plain Haskell version to CRC32 benchmark.

--- benchmarks/crc32/crc32.hs
+++ benchmarks/crc32/crc32.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP, BangPatterns #-}
 import Haste.Aplite
 import Haste.Performance
 import Numeric
@@ -42,15 +42,57 @@ crc32 = aplite tuning $ \tbl len buf -> do
       x' <- getArr ((crc `xor` x) .&. 0xFF) tbl
       return $ (crc `shiftRL` 8) `xor` x'
 
+shiftRL' :: Int32 -> Int -> Int32
+shiftRL' a b = fromIntegral ((fromIntegral a :: Word32) `shiftR` b) :: Int32
+
+-- standard Haskell version
+mktable :: IO CRCTable
+mktable = do
+  tbl <- newArray_ (0, 255)
+  forM_ [0..255] $ \i -> do
+    c <- foldr (.) id (replicate 8 step) (pure i)
+    writeArray tbl i c
+  return tbl
+  where
+    step :: IO Int32 -> IO Int32
+    step mc = mc >>= \c -> do
+      if c .&. 1 == 1
+        then pure $ fromIntegral (-306674912) `xor` (c `shiftRL'` 1)
+        else pure $ c `shiftRL'` 1
+
+crc32hs :: CRCTable -> Int32 -> IOUArray Int32 Int32 -> IO Int32
+crc32hs tbl len buf = go (-1) 0
+  where
+    go !crc !i
+      | i < len   = do
+          crc' <- step crc i
+          go crc' (i+1)
+      | otherwise = return (crc `xor` 0xFFFFFFFF)
+    
+    step :: Int32 -> Int32 -> IO Int32
+    step crc i = do
+      x <- readArray buf i
+      x' <- readArray tbl ((crc `xor` x) .&. 0xFF)
+      return $ (crc `shiftRL'` 8) `xor` x'
+
 testlen = 100000000
 
 main = do
   tbl <- newArray_ (0,255)
   buf <- newArray_ (0, testlen-1)
   signed_crc_table tbl
-  x <- crc32 tbl testlen buf
+--  x <- crc32 tbl testlen buf
   t <- now
-  x <- crc32 tbl testlen buf
+  x <- crc32hs tbl testlen buf
   t' <- now
   print x
   print (t'-t)
+
+{-
+-- standard Haskell main
+main = do
+  tbl <- mktable
+  buf <- newArray_ (0, testlen-1)
+  x <- crc32hs tbl testlen buf
+  print x
+-}
